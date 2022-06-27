2adefe223ab8b113d9ca1a0eefa8cd95eb476af1
Michael Ibrahim <michael.ibrahim042@gmail.com>
2022-06-27 21:31:23 +0200

    Add MCAL modules

---
+++ 01_MCAL/02_Systick/Systick_cfg.h
@@ -0,0 +1,16 @@
+/* **************************************************************************************
+**       Author      :	Michael S. Ibrahim
+**       Date        :	March 23, 2022
+**       Version     :	V01
+**       SWC         :	Systick
+**       Description :	Systick timer module files
+** **************************************************************************************/
+#ifndef _SYSTICK_CFG_H
+#define _SYSTICK_CFG_H
+  /* ********************* Configuration MACROS ****************************************** */
+  /* The used clock */
+  /* enter used clock in 'MHZ' */
+  #define SYSTICK_u8CLOCK                       16
+  /* ************************************************************************************* */
+
+#endif
---
+++ 01_MCAL/02_Systick/Systick_prv.h
@@ -0,0 +1,33 @@
+/* **************************************************************************************
+**       Author      :	Michael S. Ibrahim
+**       Date        :	March 23, 2022
+**       Version     :	V01
+**       SWC         :	Systick
+**       Description :	Systick timer module files
+** **************************************************************************************/
+#ifndef _SYSTICK_PRV_H
+#define _SYSTICK_PRV_H
+/* ******************************* MACROS  ********************************************* */
+/* CLk Source Mask */
+#define SYSTICK_u32CLK_SRC            0x04
+/* Interrupt MASK */
+#define SYSTICK_u32INT                0x02
+/* Enable Mask */
+#define SYSTICK_u32ENABLE             0x01
+/* Max reload value */
+#define SYSTICK_u32MAX_RELOAD         0x1000000
+/* Value counter clear mask */
+#define SYSTICK_u32VAL_CLEAR          0x00
+
+/* ************************************************************************************* */
+
+
+
+
+
+
+
+
+
+
+#endif
