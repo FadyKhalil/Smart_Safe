7b4601b91092ec4a1be20179b2e37ed2e4a814ce
Michael Ibrahim <michael.ibrahim042@gmail.com>
2022-06-28 20:57:47 +0200

    Add project V1.0

---
+++ 00_OS/02_Config/OS_cfg.h
@@ -0,0 +1,17 @@
+/* **************************************************************************************
+**       Author      :	Michael S. Ibrahim
+**       Date        :	May 27, 2022
+**       Version     :	V01
+**       SWC         :	OS
+**       Description :	OS files
+** **************************************************************************************/
+#ifndef _OS_CFG_H
+#define _OS_CFG_H
+  /* ///////////////////////////////// MACROS ////////////////////////////// */
+  #define OS_u8TASKS_MAX                      ((u8)(10))
+  #define OS_u8SEMAPHORES_MAX                 ((u8)(10))
+  #define OS_u8IDLE_TASK_STACK_SIZE           ((u8)(85))
+  /* ****Caution**** : Don't forget to configure the systick clock with your system clock */
+  #define OS_u16TICK_TIME                     ((u16)(1))
+  /* /////////////////////////////////////////////////////////////////////// */
+#endif
